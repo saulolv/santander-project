5415bc376c56c2fca4454d76095b71bda72d92d4
saulolv <saulosantos229@gmail.com>
2024-07-10 00:50:41 -0300

    Initial commit

---
+++ src/main/java/study/santanderproject/SantanderProjectApplication.java
@@ -0,0 +1,13 @@
+package study.santanderproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/santanderproject/SantanderProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package study.santanderproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
